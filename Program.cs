9715984d8a5c3baacc4bf6042a3905d180ecbd9b
Enduren <dtennison79@gmail.com>
2021-10-29 13:32:28 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodOverload
+{
+	class Program
+	{
+		
+
+		public static int Add(int num1, int num2)
+		{
+			return num1 + num2;
+		}
+
+		public static decimal Add(decimal num1, decimal num2)
+		{
+			return num1 + num2;
+		}
+
+		public static string Add(int num1, int num2, bool isCorrect)
+		{
+			var sum = num1 + num2;
+
+			if (isCorrect == true && sum > 1)
+			{
+				return $"{sum} dollars";
+			}
+			else if (isCorrect == true && sum == 1)
+			{
+				return $"{sum} dollar";
+			}
+			else if (isCorrect == true && sum < 1)
+			{
+				return $"{sum} dollars";
+			}
+			else
+			{
+				return sum.ToString();
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var x = 29;
+			var y = 55;
+
+			var answer = Add(x, y);
+
+			var a = 32.0m;
+			var b = 45.0m;
+
+			var decimalAnswer = Add(a, b);
+
+			var thirdAnswer = Add(0, 0, true);
+
+			Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+			Console.WriteLine(thirdAnswer);
+		}
+	}
+}
